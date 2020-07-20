16974e2b53b6ff9ad820fc79a1ad665a611ed18a
HVjay <42661185+HVjay@users.noreply.github.com>
2020-07-20 14:23:30 -0400

    Add files via upload

--- phase2.m
+++ phase2.m
@@ -2,6 +2,20 @@ file_name='E_octave_new_new.wav'; % output signal of phase 1
 
 
 [y, Fs] = audioread(file_name);
+figure (3);
+plot (y);
+
+y_fft = fft(y);
+L =length(y_fft);
+NFFT = 2^nextpow2(L); % Next power of 2 from length of y
+Y = fft(y,NFFT)/L;
+f = Fs/2*linspace(0,1,NFFT/2+1);
+figure(1);
+plot(f,2*abs(Y(1:NFFT/2+1)));
+title('Single-Sided Amplitude Spectrum of original y(t)');
+xlabel('Frequency (Hz)');
+ylabel('|Y(f)|');
+
 %{
 [time_points,n]=size(y);
 if (n==2)
@@ -13,49 +27,71 @@ else
    new_y=y;
 end
 
+
 [P,Q] = rat(Fs/16000);
 down_sampled=resample(new_y,Q,P);
-filename='E_octave_new_new.wav';
+filename='running_new_new.wav';
 audiowrite(filename,down_sampled,16000);
 %}
 
-passband=zeros(8,length(y));
-Fc1=100;
-Fc2=1000;
-k=size(passband);
+passbank=zeros(8,length(y));
+Fc1=100; % Start frequency%
+Fc2=1000; % End frequency%
+k=size(passbank); 
 rows=k(1);
-N=8;
+N=100;
 flag='scale';
 Beta=6;
-fir_kaiser=FIR_kaiser(N,Fs,Fc1,Fc2,flag,Beta);
-filtered_signal=filter(fir_kaiser,y);
+Hd=FIR_kaiser(N,Fs,Fc1,Fc2,flag,Beta);
+filtered_signal=filter(Hd,y);
 
-    
-L= length(filtered_signal);
+L = length(filtered_signal);
 NFFT = 2^nextpow2(L); % Next power of 2 from length of y
 Y = fft(filtered_signal,NFFT)/L;
 f = Fs/2*linspace(0,1,NFFT/2+1);
 figure(2);
 plot(f,2*abs(Y(1:NFFT/2+1)));
-title('Single-Sided Amplitude Spectrum of y(t)');
+title('Single-Sided Amplitude Spectrum of filtered y(t)');
 xlabel('Frequency (Hz)');
 ylabel('|Y(f)|');
 
-%{
+
 for n=1:rows
-    N=8;
+    N=100;
     flag='scale';
     Beta=6;
+    disp(Fc1);
     fir_kaiser=FIR_kaiser(N,Fs,Fc1,Fc2,flag,Beta);
    % y_fft=fft(y);
     filtered_signal=filter(fir_kaiser,y);
+    passbank(n,:)=filtered_signal;
+    Fc1=Fc2;
+    Fc2=Fc2+1000-1;
     %plot(filtered_signal)
-    
-    L= length(filtered_signal);
-    NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-    Y = fft(filtered_signal,NFFT)/L;
-    plot(Y)
+  
    % fft_new=fft(filtered_signal);
     %plot(fft_new);
 end
-%}
+
+
+envelope=abs(passbank);
+all_envelope=zeros(8,length(y));
+for i=1:8
+    Hd = lpf(Fs, 400, 10);
+    all_envelope(i,:) = filter(Hd, envelope(i,:));
+end
+
+L = length(filtered_signal);
+NFFT = 2^nextpow2(L); % Next power of 2 from length of y
+Y = fft(filtered_signal,NFFT)/L;
+f = Fs/2*linspace(0,1,NFFT/2+1);
+figure(2);
+plot(f,2*abs(Y(1:NFFT/2+1)));
+title('Single-Sided Amplitude Spectrum of filtered y(t)');
+xlabel('Frequency (Hz)');
+ylabel('|Y(f)|');
+plot(all_envelope(1, :)), xlabel('Sample Number'),ylabel('Amplitude'),title('Filtered Sound waveform (100 - 460)');
+plot(all_envelope(22, :)), xlabel('Sample Number'),ylabel('Amplitude'),title('Filtered Sound waveform (7640 -8000)');
+
+
+
