3b8ae45117b96c7cf52b919925b8462f1b96cd83
HVjay <42661185+HVjay@users.noreply.github.com>
2020-07-26 18:10:12 -0400

    Add files via upload

--- phase_2_new.m
+++ phase_2_new.m
@@ -2,125 +2,70 @@ file_name='E_octave_new_new.wav'; % output signal of phase 1
 
 [y, Fs] = audioread(file_name);
 
-y_fft = fft(y);
-L =length(y_fft);
-NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-Y = fft(y,NFFT)/L;
-f = Fs/2*linspace(0,1,NFFT/2+1);
-figure(1);
-plot(f,2*abs(Y(1:NFFT/2+1)));
-title('Original y(t)');
-xlabel('Frequency (Hz)');
-ylabel('|Y(f)|');
-
-%{
-[time_points,n]=size(y);
-if (n==2)
-    new_y=zeros(time_points,1);
-    for k=1:time_points
-        new_y(k,1)=y(k,1)+y(k,2);
-    end
-else
-   new_y=y;
-end
-
-[P,Q] = rat(Fs/16000);
-down_sampled=resample(new_y,Q,P);
-filename='E_octave_new_new.wav';
-audiowrite(filename,down_sampled,16000);
-%}
-
-%{
-Fc1=100; % Start frequency%
-Fc2=1000; % End frequency%
-N=100;
-flag='scale';
-Beta=6;
-Hd=FIR_Kaiser2(N,Fs,Fc1,Fc2,flag,Beta);
-filtered_signal=filter(Hd,y);
-
-L = length(filtered_signal);
-NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-Y = fft(filtered_signal,NFFT)/L;
-f = Fs/2*linspace(0,1,NFFT/2+1);
-figure(2);
-plot(f,2*abs(Y(1:NFFT/2+1)));
-title('Filtered y(t)');
-xlabel('Frequency (Hz)');
-ylabel('|Y(f)|');
-%}
-
+%create passband bank and set initial frequency range
 passbank=zeros(8,length(y));
-Fc1=100; % Start frequency%
-Fc2=1000; % End frequency%
+cutoff_1=100; % Start frequency%
+cutoff_2=1000; % End frequency%
 k=size(passbank); 
 rows=k(1);
 
 
 for n=1:rows
-    N=100;
+    N=25;
     flag='scale';
     Beta=6;
     
-    
-    fir_kaiser=FIR_Kaiser2(N,Fs,Fc1,Fc2,flag,Beta);
-   % y_fft=fft(y);
+    fir_kaiser=FIR_Kaiser2(N,Fs,cutoff_1,cutoff_2,flag,Beta);
    
-    tic
+    %tic
     filtered_signal=filter(fir_kaiser,y);
-    %plot(filtered_signal)
-    toc
+    %toc
     
+    %add filtered signal to passband bank and update frequency range
     passbank(n,:)=filtered_signal;
-    Fc1=round(Fc2,-3);
-    Fc2=round(Fc2+1000,-3)-1;
+    cutoff_1=round(cutoff_2,-3);
+    cutoff_2=round(cutoff_2+1000,-3)-1;
     
 end
 
+%plot of first and last channels
+figure(1);
+plot(passbank(1,:));
+title('Filtered Sound waveform (100 - 1000 Hz)');
+xlabel('Time points');
+ylabel('Gain (dB)');
 
-L = length(passbank(1, :));
-NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-Y = fft(passbank(1, :),NFFT)/L;
-f = Fs/2*linspace(0,1,NFFT/2+1);
-figure(3);
-plot(f,2*abs(Y(1:NFFT/2+1)));
-title(' Amplitude Spectrum of Filtered Sound waveform (100 - 1000)');
-xlabel('Frequency (Hz)');
-ylabel('Amplitude');
+figure(2);
+plot(passbank(8, :));
+title('Filtered Sound waveform (7000 - 7999 Hz)');
+xlabel('Time points');
+ylabel('Gain (dB)');
 
-
-L = length(passbank(8, :));
-NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-Y = fft(passbank(8, :),NFFT)/L;
-f = Fs/2*linspace(0,1,NFFT/2+1);
-figure(4);
-plot(f,2*abs(Y(1:NFFT/2+1)));
-title(' Amplitude Spectrum of Filtered Sound waveform (7000 - 7999)');
-xlabel('Frequency (Hz)');
-ylabel('Amplitude');
-
+%create envelope and channels of each envelope
 envelope=abs(passbank);
-
-figure(10)
-plot(envelope(1,:));
-
 all_envelope=zeros(8,length(y));
 for i=1:8
-    Hd = lpf(Fs, 400, 10);
+    N=4;
+    sampling_freq = 16000;
+    cutoff_freq=400;
+    Hd = butter_filter(N,sampling_freq,cutoff_freq);
+    
+    %add filtered envelope 
     all_envelope(i,:) = filter(Hd, envelope(i,:));
 end
 
-%[yupper,ylower]=envelope(
-
-figure(5);
+%plot envelopes
+figure(3);
 plot(all_envelope(1,:));
+title('Envelope waveform (100 - 1000 Hz)');
+xlabel('Time points');
+ylabel('Gain (dB)');
 
-L = length(all_envelope(1, :));
-NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-Y = fft(all_envelope(1, :),NFFT)/L;
-f = Fs/2*linspace(0,1,NFFT/2+1);
-figure(6);
-plot(f,2*abs(Y(1:NFFT/2+1)));
-title(' Amplitude Spectrum of the envelope (100- 1000)');
-xlabel('Frequency (Hz)');
-ylabel('Amplitude');
+
+figure(4);
+plot(all_envelope(8, :));
+title('Envelope waveform(7000 - 7999 Hz)');
+xlabel('Time points');
+ylabel('Gain (dB)');
+
+
